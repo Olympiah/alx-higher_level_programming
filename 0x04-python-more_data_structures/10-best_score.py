b325bc7069d81c6304f6329cf9b9c50960289484
Olympiah Otieno <87874970+Olympiah@users.noreply.github.com>
2022-12-08 18:41:04 +0300

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    biggest = 0
-    for key, value in a_dictionary.items():
-        if value > biggest:
-            biggest = value
-    for key, value in a_dictionary.items():
-        if value == biggest:
-            return 
+def best_score(my_dict):
+    return max(my_dict, key=my_dict.get) if my_dict else None
